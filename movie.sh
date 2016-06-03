ba61d15d1299a7b38339978285c36b838f1f5a14
Sushant <sushcool94@gmail.com>
2016-06-04 05:11:27 +0530

    Sorting Complete

--- movie.sh
+++ movie.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 function getrating()
 {
-curl "http://www.omdbapi.com/?t=$1&y=&plot=short&r=xml" > mov.txt
+curl -s  "http://www.omdbapi.com/?t=$1&y=&plot=short&r=xml" > mov.txt
 grep -o imdbRating=".*" mov.txt > submov.txt
 input="submov.txt"
 echo ""
@@ -24,45 +24,22 @@ do
 done < "$input"
 rm mov.txt
 rm submov.txt
-rate=$(printf %.2f $rating)
+rate=$(printf %.1f $rating)
 echo $rate
 }
 
-
+rm out.txt
 ls movies > movlist.txt
 sed 's/_/./g' movlist.txt > mov2.txt
 input2="mov2.txt"
+echo Rating '     '  Novie >> out.txt
 while IFS= read -r var2
 do
-  echo $var2
+  #echo $var2
   rating=$( getrating $var2 )
-  echo $rating
+  #echo $rating
+  echo $rating '      '  $var2 >> out.txt
 done < "$input2"
+sort -k 1 -r out.txt
 exit 1
 
-function getratingi()
-{
-curl "http://www.omdbapi.com/?t=$1r&y=&plot=short&r=xml" > mov.txt
-grep -o imdbRating=".*" mov.txt > submov.txt
-input="submov.txt" 
-echo ""
-ctr=0
-rating=""
-while IFS= read -r -n1 var
-do
-  if [ $ctr -eq 2 ]
-  then 
-  	break
-  elif [ $var == '"' ]
-  then
-  	ctr=$(( $ctr + 1 ))
-  elif [ $ctr -eq 1 ]
-  then
- 	rating="$rating$var"
-  else 
-	continue
-  fi
-done < "$input"
-echo $rating
-return 0
-}
