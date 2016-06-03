4f8e7699475509372ef9c3f15de0c263f68b6fe0
Sushant <sushcool94@gmail.com>
2016-06-04 05:25:31 +0530

    Output and Documentation

--- movie.sh
+++ movie.sh
@@ -1,12 +1,18 @@
 #!bin/bash
+
 function getrating()
 {
+#Fetch tuple from the Omdb API
 curl -s  "http://www.omdbapi.com/?t=$1&y=&plot=short&r=xml" > mov.txt
+#Getting the Line with the information
 grep -o imdbRating=".*" mov.txt > submov.txt
 input="submov.txt"
 echo ""
 ctr=0
 rating=""
+#Extracting the rating. Since the format is XYZ= "x"
+#Track the opening and closing quotes to get x which is rating
+#ctr tracks number of " encountered
 while IFS= read -r -n1 var
 do
   if [ $ctr -eq 2 ]
@@ -24,11 +30,13 @@ do
 done < "$input"
 rm mov.txt
 rm submov.txt
+#Convert String to Float
 rate=$(printf %.1f $rating)
 echo $rate
 }
 
 rm out.txt
+#Fetching Movies and removing _
 ls movies > movlist.txt
 sed 's/_/./g' movlist.txt > mov2.txt
 input2="mov2.txt"
@@ -40,6 +48,8 @@ do
   #echo $rating
   echo $rating '      '  $var2 >> out.txt
 done < "$input2"
+#Above, a file is created with 2 coloumns of Movie and rating
+#Then it is sorted
 sort -k 1 -r out.txt
 exit 1
 
