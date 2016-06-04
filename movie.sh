516dabc4e8b55cad33a9c41f64d0fd18f142d65f
skthinks <sushcool94@gmail.com>
2016-06-04 12:31:10 +0530

    Some small fixes

--- movie.sh
+++ movie.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 
 readonly TEMP_PATH="$TMPDIR/imdb"
+rm -rf $TEMP_PATH
 mkdir $TEMP_PATH      # Making a Folder to Dump Files
 readonly NUM_ARGUMENTS=$#
 readonly DIRECTORY_NAME=$1
@@ -16,8 +17,8 @@ function add_style(){
 
 function get_rating(){
     local movie_name=$1
-    local rating_in_string=$( curl -s  "http://www.omdbapi.com/?t=$movie_name&y=&plot=short&r=xml"  \
-       | grep -o imdbRating=".*"  \
+    local rating_in_string=$(curl -s  "http://www.omdbapi.com/?t=$movie_name&y=&plot=short&r=xml" \
+       | grep -o imdbRating=".*" \
        | cut -c 13,14,15)
     local rating_in_float=$(printf %.1f $rating_in_string)     # Convert Rating String to Float
     echo $rating_in_float
@@ -27,18 +28,16 @@ function get_rating(){
 function main(){
    
     # There should be exactly one argument i.e the address path
-    if [ $NUM_ARGUMENTS != 1 ]
-    then
-	    add_style "This application takes one argument : Movie Directory" 1
+    if [ $NUM_ARGUMENTS != 1 ]; then
+        add_style "This application takes one argument : Movie Directory" 1
         rm -rf $TEMP_PATH
-	    exit 4
+        exit 4
     fi
     # Fetching Movies and removing 
     ls $DIRECTORY_NAME > "$TEMP_PATH/movlist.txt"
-    local movie_count=$( cat "$TEMP_PATH/movlist.txt" \
-        | wc -l )
-    if [ $movie_count == 0 ]
-    then
+    local movie_count=$(cat "$TEMP_PATH/movlist.txt" \
+        | wc -l)
+    if [ $movie_count == 0 ]; then
         add_style "Directory is either Invalid or Empty" 1 
         rm -rf $TEMP_PATH
         exit 4
@@ -46,12 +45,11 @@ function main(){
     sed 's/_/./g' "$TEMP_PATH/movlist.txt" > "$TEMP_PATH/temp_clean_mov.txt"
     sed 's/ /./g' "$TEMP_PATH/temp_clean_mov.txt" > "$TEMP_PATH/clean_mov.txt"
     echo Rating '     '  Novie >> "$TEMP_PATH/out.txt"  
-    while IFS= read -r movie_name
-    do
+    while IFS= read -r movie_name; do
         rating=$( get_rating $movie_name )
         if [ $rating == 0.0 ]
         then
-	        rating="N/A"
+            rating="N/A"
         fi
         echo $rating '      '  $movie_name >> "$TEMP_PATH/out.txt"
     done < "$TEMP_PATH/clean_mov.txt"
