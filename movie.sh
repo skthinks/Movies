ba0cfea4c58c9972391ac8cece3984c456e81f21
skthinks <sushcool94@gmail.com>
2016-06-04 15:28:30 +0530

    No files in this

--- movie.sh
+++ movie.sh
@@ -1,8 +1,5 @@
 #!bin/bash
 
-readonly TEMP_PATH="$TMPDIR/imdb"
-rm -rf $TEMP_PATH
-mkdir $TEMP_PATH      # Making a Folder to Dump Files
 readonly NUM_ARGUMENTS=$#
 readonly DIRECTORY_NAME=$1
 
@@ -30,38 +27,33 @@ function main(){
     # There should be exactly one argument i.e the address path
     if [ $NUM_ARGUMENTS != 1 ]; then
         add_style "This application takes one argument : Movie Directory" 1
-        rm -rf $TEMP_PATH
         exit 4
     fi
     # Fetching Movies and removing 
-    ls $DIRECTORY_NAME > "$TEMP_PATH/movlist.txt"
-    local movie_count=$(cat "$TEMP_PATH/movlist.txt" \
+    movie_list=$(ls $DIRECTORY_NAME \
+     | grep "" | \
+     sed 's/_/./g; s/ /./g')
+    local movie_count=$(echo $movie_list \
         | wc -l)
     if [ $movie_count == 0 ]; then
         add_style "Directory is either Invalid or Empty" 1 
-        rm -rf $TEMP_PATH
         exit 4
     fi
-    sed 's/_/./g' "$TEMP_PATH/movlist.txt" > "$TEMP_PATH/temp_clean_mov.txt"
-    sed 's/ /./g' "$TEMP_PATH/temp_clean_mov.txt" > "$TEMP_PATH/clean_mov.txt"
-    echo Rating '     '  Novie >> "$TEMP_PATH/out.txt"  
-    while IFS= read -r movie_name; do
+    output="Rating      Movie\n"
+    for movie_name in $movie_list; do
         rating=$( get_rating $movie_name )
-        if [ $rating == 0.0 ]
-        then
+        if [ $rating == 0.0 ]; then
             rating="N/A"
         fi
-        echo $rating '      '  $movie_name >> "$TEMP_PATH/out.txt"
-    done < "$TEMP_PATH/clean_mov.txt"
-    clear
+        output="$output $rating       $movie_name\n"
+    done
     # Above, a file is created with 2 coloumns of Movie and rating
     # Then it is sorted
-    sort -k 1 -r "$TEMP_PATH/out.txt"
+    echo -e $output | sort -k 1 -r 
     tput setaf 1
     tput bold
     printf  "\n\nN/A: The application was unable to fetch your movie. We regret this deeply\n"
     tput sgr 0
-    rm -rf $TEMP_PATH
     exit 0
 }
 
